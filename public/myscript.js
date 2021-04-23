434ae814e527f5069ddff1f84def0e1836e02df4
Francesco Pantaleoni <pantaleoni.francesco@gmail.com>
2021-04-23 16:29:06 +0200

    login funzionante

--- public/myscript.js
+++ public/myscript.js
@@ -3,20 +3,18 @@
 //       $("html, body").animate({ scrollTop: 0 }, "slow");
 //    }
 // )
-
 function Tornasu(){
    setTimeout(  
       function(){
          $("html, body").animate({ scrollTop: 0}, "slow")
       },290
    );
-
 }
 // function Tornasu(){
 //    $("html, body").animate({scrollTop: 0}, { duration: 2000}, 'easeout');
 // }
 
-var sumbitFormChecks = [{
+var inputNuovoForm = [{
    "query": "input[type=text]",
    "validators": [validateRequired, validateLength]
    }, {
@@ -27,90 +25,48 @@ var sumbitFormChecks = [{
    "validators": [validateRequired, validateLength]
 }  ]
 
-var loginFormChecks = [{
+var inputLoginAdmin = [{
       "query": "input[type=text], input[type=password]",
       "validators": [validateRequired, validateLength]
 }  ]
 
-/**
-* @summary funzione che chiama $callback quando document è caricato
-* @param callback, funzione senza parametri
-*/
-function ready(callback){
-// in case the document is already rendered
-if (document.readyState!='loading') callback();
-else document.addEventListener('DOMContentLoaded', callback);
-}
-
-ready(function(){document.onsubmit = validateForm();})
-
-/**
-* @summary funzione che effettua il controllo e visualizza i messaggi d'errore
-*          sui campi del form, adattandosi al tipo di form: invio oppure login.
-* @param event, evento che scatena l'avvio della funzione. Non usato
-* @return indicazione se il form è valido oppure no.
-*/
-function validateForm (event){
-deleteErrorMessages();
-var form, checks, isLogin = document.getElementById('login') !== null;
+function validateForm (){
+eliminaErrori();
+var form, checks
+var isLogin = document.getElementById('login') !== null;
 form = isLogin ? document.forms['login'] : document.forms['invio'];
-checks = isLogin ? loginFormChecks : sumbitFormChecks;
-return checks.map((check) => executeCheck(check, form)).reduce((previous, current) => previous && current);
+checks = isLogin ? inputLoginAdmin : inputNuovoForm;
+return checks.map((check) => validate(check, form)).reduce((previous, current) => previous && current);
 }
 
-/**
-* @summary funzione che prende in input un controllo e lo applica a tutti i campi del form
-*          consoni e ne mostra gli eventuali errori
-* @param check, oggetto con i seguenti campi
-*             check.query:      query css che individua quali campi vanno controllati 
-*             check.validators: lista di funzioni che rappresentano i controlli da effettuare
-*                               per i campi individuati da $check.query.
-* @return indicazione se tutti i campi trovati da $check.query rispettano i controlli di $check.validators
-*/
-function executeCheck(check, form){
+function validate(check, form){
 const inputs = Array.from(form.querySelectorAll(check.query));
 const wrong_inputs = inputs.filter((input) => !checkInput(input, check.validators));
-wrong_inputs.forEach(input => showErrorMessage(input));
+wrong_inputs.forEach(input => mostraErrori(input));
 return wrong_inputs.length === 0;
 }
 
-/**
-* @summary funzione che mostra il messagio d'errore, se non già presente pensato per $input
-* @param $input, campo di cui mostrarne l'errore
-*/
-function showErrorMessage(input){
+function mostraErrori(input){
 var label = document.querySelector("label[for=%s]".replace(/%s/, input.id));
 if(label !== null && label.getElementsByClassName("error").length === 0) {
 var error = document.createElement("strong");
 error.className = "error";
 error.setAttribute("role", "alert");
+error.setAttribute("aria-live", "polite");
 error.appendChild(document.createTextNode(" - " +label.getAttribute("data-error-msg")));
 label.appendChild(error);
 }
 }
 
-/**
-* @summary funzione che elimina tutti i messaggi d'errore presenti nel form
-*/
-function deleteErrorMessages(){
+function eliminaErrori(){
 var errors = document.getElementsByClassName("error");
 while(0 < errors.length) errors[0].remove();
 }
 
-/**
-* @summary funzione che esegue tutti i controlli su uno specifico input
-* @param input, campo da controllare
-* @param validators, lista di funzioni che dicono se $input è valido oppure no
-* @return indicazione se $input è valido per tutti i $validators 
-*/
 function checkInput(input, validators){
 return validators.reduce((previousValidity, validator) => previousValidity && validator(input),true);
 }
 
-/**
-* @param input campo da controllare
-* @return indicazione se $input rispetta la lunghezza indicata negli attributi minlength, maxlength
-*/
 function validateLength(input){
 const min = input.getAttribute("minlength");
 const max = input.getAttribute("maxlength");
@@ -118,11 +74,6 @@ return ((min === null) || (input.value.length >= min))
 && ((max === null) || (input.value.length <= max));
 }
 
-/**
-* @param input campo da controllare
-* @return indicazione se l'eventuale file inserito
-*         in $input è un immagine sotto il MB
-*/
 function validateImageFile(input) {
 const files = input.files;
 return ((files.length === 1)
@@ -131,10 +82,6 @@ return ((files.length === 1)
 || (files.length === 0);
 }
 
-/**
-* @param input campo da controllare
-* @return indicazione se $input l'obbligatorietà indicata con l'attributo required
-*/
 function validateRequired(input) {
 return input.hasAttribute("required")? input.value != "" : true;
 }
